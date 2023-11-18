63b7b665ace97aeae535649c41864f4674df16d6
kevinss09 <kevinsugeng090@gmail.com>
2023-11-17 19:03:07 -0800

    Add menu

--- src/JSON/sushi.js
+++ src/JSON/sushi.js
@@ -3,7 +3,7 @@ export const weeklySpecialJson = [
 		id: "1",
 		image: "images/week1Special.png",
 		name: "Temaki Sushi",
-		price: "$19.99",
+		price: "$1.99",
 	},
 	{
 		id: "2",
@@ -22,5 +22,65 @@ export const weeklySpecialJson = [
 		image: "images/week4Special.png",
 		name: "Nigiri Sushi",
 		price: "$2.00",
+	},
+];
+
+export const popularJson = [
+	{
+		id: "1",
+		image: "images/Popular1.png",
+		name: "Temaki Sushi",
+		price: "$2.00",
+	},
+	{
+		id: "2",
+		image: "images/Popular2.png",
+		name: "Unagi Roll",
+		price: "$4.49",
+	},
+	{
+		id: "3",
+		image: "images/Popular3.png",
+		name: "Dragon Roll",
+		price: "$5.50",
+	},
+	{
+		id: "4",
+		image: "images/Popular4.png",
+		name: "BC Roll",
+		price: "$5.00",
+	},
+	{
+		id: "5",
+		image: "images/Popular5.png",
+		name: "Dynamite Sushi",
+		price: "$3.50",
+	},
+];
+
+export const partyJson = [
+	{
+		id: "1",
+		image: "images/Party1.png",
+		name: "Party Tray A",
+		people: "(For 2-3 people)",
+		desc: "(42 pcs) California, Dynamite, Spicy Tuna, BC, Tuna, Salmon, Kappa Rolls",
+		price: "$26.00",
+	},
+	{
+		id: "2",
+		image: "images/Party2.png",
+		name: "Party Tray B",
+		people: "(For 3-4 people)",
+		desc: "(48 pcs) Dynamite, Spicy Tuna, BC, Philadelphia, Alaska, Tuna, Salmon, Kappa Rolls",
+		price: "$35.00",
+	},
+	{
+		id: "3",
+		image: "images/Party3.png",
+		name: "Party Tray C",
+		people: "(For 3-4 people)",
+		desc: "(40 pcs) California, Dynamite, Spicy Tuna, BC, Philadelphia Rolls, 2 pcs each of Tuna, Salmon, Ebi, Tamago Nigiri",
+		price: "$39.00",
 	},
 ];
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 import headerImage from "../assets/images/HeaderImage.png";
 import headerSideImage from "../assets/images/HeaderSideImage.png";
 import aboutImg from "../assets/images/aboutImage.png";
-import { weeklySpecialJson } from "../JSON/sushi";
+import { weeklySpecialJson, popularJson, partyJson } from "../JSON/sushi";
+import fullMenu from "../assets/images/FullMenu.png";
 
 export default function Home() {
 	return (
@@ -57,7 +58,10 @@ export default function Home() {
 					<div className="w-full flex flex-row justify-between h-[550px]">
 						{weeklySpecialJson.map((sushi, index) => {
 							return (
-								<div className="flex flex-col items-center justify-between">
+								<div
+									className="flex flex-col items-center justify-between"
+									key={index}
+								>
 									<img src={sushi.image} alt="" className={`Image ${index}`} />
 									<h3 className="text-white text-3xl tracking-wider">
 										{sushi.name}
@@ -76,11 +80,67 @@ export default function Home() {
 					MENU
 				</h1>
 				<div>
-					<h3 className="font-tropical text-[50px] text-white">
+					<h3 className="font-tropical text-[60px] text-white">
 						Popular dishes
 					</h3>
-					<div className="flex flex-row justify-between items-center">
-						<div> </div>
+					<div className="grid grid-cols-5 grid-rows-1 gap-20 my-10">
+						{popularJson.map((popular, index) => {
+							return (
+								<div className="col-span-1 bg-white flex flex-col" key={index}>
+									<img
+										src={popular.image}
+										alt={popular.name}
+										className="w-full object-cover"
+									/>
+									<div className="py-6 flex flex-col justify-between h-[125px]">
+										<h2 className="text-center font-medium text-2xl tracking-wider">
+											{popular.name}
+										</h2>
+										<h5 className="text-center font-light text-xl">
+											{popular.price}
+										</h5>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+				<div>
+					<h3 className="font-tropical text-[60px] text-white">Party Tray</h3>
+					<div className="grid grid-cols-3 grid-rows-1 my-10 gap-16">
+						{partyJson.map((party, index) => {
+							return (
+								<div className="col-span-1 bg-white flex flex-col" key={index}>
+									<img
+										src={party.image}
+										alt={`party ${party.id}`}
+										className="w-full object-cover"
+									/>
+									<div className="h-[250px] py-6 px-7 flex flex-col items-center justify-between">
+										<div>
+											<h2 className="text-center font-semibold text-2xl tracking-wider">
+												{party.name}
+											</h2>
+											<h5 className="text-center font-sans font-light text-lg tracking-wider mt-1">
+												{party.people}
+											</h5>
+										</div>
+										<h4 className="text-center text-lg font-sans font-light tracking-wider">
+											{party.desc}
+										</h4>
+										<h5 className="font-semibold text-xl tracking-wider">
+											{party.price}
+										</h5>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+				<div>
+					<h3 className="font-tropical text-[60px] text-white">Full Menu</h3>
+					<div className="my-10">
+						<img src={fullMenu} alt="" className="w-full h-full object-cover" />
 					</div>
 				</div>
 			</div>
